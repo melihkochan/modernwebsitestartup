d5218aa5dc2be1f8c059bf42d3821744d635931d
melihkochan <melihkochan@users.noreply.github.com>
2026-06-27 00:55:59 +0300

    chore: configure tsconfig and types for supabase functions

---
+++ supabase/functions/global.d.ts
@@ -0,0 +1,16 @@
+declare namespace Deno {
+  export interface ServeOptions {
+    port?: number;
+    hostname?: string;
+    onListen?: (params: { port: number; hostname: string }) => void;
+    onError?: (error: unknown) => Response | Promise<Response>;
+  }
+
+  export function serve(
+    handler: (request: Request, info: any) => Response | Promise<Response>
+  ): void;
+  export function serve(
+    options: ServeOptions,
+    handler: (request: Request, info: any) => Response | Promise<Response>
+  ): void;
+}
